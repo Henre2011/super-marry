7b706e028f84b1b7b52c87788986b633c02bee8c
Henre2011 <142222761+Henre2011@users.noreply.github.com>
2024-01-07 14:54:43 +0800

    Create fcousclock.py

---
+++ fcousclock.py
@@ -0,0 +1,17 @@
+import time
+import tkinter as tk
+
+def update_clock():
+    current_time = time.strftime('%H:%M:%S')
+    clock_label.config(text=current_time)
+    clock_label.after(1000, update_clock)
+
+root = tk.Tk()
+root.title("专注时钟")
+
+clock_label = tk.Label(root, font=('Arial', 80), fg='black', bg='white')
+clock_label.pack(padx=50, pady=50)
+
+update_clock()
+
+root.mainloop()
